5d8b00fe0041b1f540697ae7ddd4567fd1baf330
schlanjo <schlanjo@gmail.com>
2021-07-10 16:27:22 -0600

    Set up initial structure of tee time grid

--- src/components/TeeTime/TeeTime.js
+++ src/components/TeeTime/TeeTime.js
@@ -3,13 +3,17 @@ import './TeeTime.css'
 const TeeTime = () => {
   return (
     <div className='tee-time'>
-      <div className='course-name'>
-        <h3>Course</h3>
-        <p>Augusta National Golf Club</p>
-      </div>
-      <div className='host-name'>
-        <h3>Host</h3>
-        <p>Eric Rabun</p>
+      <h3 className='course-name'>Augusta National Golf Club</h3>
+      <div className='tt-details'>
+        <div className='host-name'>
+          <h3>Host</h3>
+          <p>Eric Rabun</p>
+        </div>
+        <div className='date-and-time'>
+          <h3>When</h3>
+          <p>July 10, 2021</p>
+          <p>@ 8:15 AM</p>
+        </div>
       </div>
     </div>
   )
