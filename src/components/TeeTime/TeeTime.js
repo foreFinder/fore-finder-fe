46dd0855896dca021a8851e034d760cab02de3f9
schlanjo <schlanjo@gmail.com>
2021-07-10 15:44:45 -0600

    Update TeeTimeContainer to utilize stylized components

--- src/components/TeeTime/TeeTime.js
+++ src/components/TeeTime/TeeTime.js
@@ -1,7 +1,23 @@
+import './TeeTime.css'
+
 const TeeTime = () => {
   return (
-    <div className='tee-time'></div>
+    <div className='tee-time'>
+      <div className='course-name'>
+        <h3>Course</h3>
+        <p>Augusta National Golf Club</p>
+      </div>
+      <div className='host-name'>
+        <h3>Host</h3>
+        <p>Eric Rabun</p>
+      </div>
+    </div>
   )
 }
 
 export default TeeTime
+
+      // <div className='invitation-actions'>
+      //   <button>Decline</button>
+      //   <button>Accept</button>
+      // </div>
--- src/components/TeeTimeContainer/TeeTimeContainer.js
+++ src/components/TeeTimeContainer/TeeTimeContainer.js
@@ -1,12 +1,31 @@
 import TeeTime from '../TeeTime/TeeTime'
+
+import styled from 'styled-components';
 import './TeeTimeContainer.css'
+
+const TTWrapper = styled.div`
+  width: 100%;
+  height: 320px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 24px;
+  border-radius: 8px;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, .2);
+`
+
+const TTLabel = styled.h2`
+  align-self: center;
+  margin: 16px 0;
+  font-size: 1.25em;
+  font-weight: 600;
+`
 
 const TeeTimeContainer = ({ title }) => {
   return (
-    <div className='tee-time-container'>
-      <h2>{title}</h2>
+    <TTWrapper>
+      <TTLabel>{title}</TTLabel>
       <TeeTime />
-    </div>
+    </TTWrapper>
   )
 }
 
