7e9113bb4a27f81c4362fc097f8643483e7039ad
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-07-10 13:57:02 -0600

    Add NavLinks to header for all views and menus

--- src/components/Header/Header.elements.js
+++ src/components/Header/Header.elements.js
@@ -1,42 +1,81 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { GiGolfTee, GiGolfFlag } from 'react-icons/gi'
+import { VscAccount } from 'react-icons/vsc'
 
 export const HeaderContainer = styled.header`
   background: #59A371;
   height: 80px;
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   font-size: 2rem;
   position: sticky;
   top: 0;
   z-index: 999;
+`;
 
-  &:hover {
-    color: red;
-  }
+export const HeaderWrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  max-width: 1600px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 50px;
+  padding-left: 50px;
+
+@media screen and (max-width: 991px) {
+  padding-right: 30px;
+  padding-left: 30px;
+}
 `;
 
 export const Logo = styled(Link)`
   color:#fff;
-  justify-self: flex-start;
+  position: static;
   text-decoration: none;
   cursor: pointer;
+  font-size: 3rem;
+  display: flex;
+  align-self: center;
+
+  @media screen and (max-width: 960px) {
+    font-size: 2.4rem;
+    margin-left: 2rem;
+
+  }
+`;
+
+export const LogoIcon = styled(GiGolfTee)`
+  margin-right:0.5rem;
 `;
 
 export const MobileIcon = styled.span`
-  height: 100%;
-  /* align-items: center; */
-  /* align-self: center; */
-  justify-self: flex-end;
+  display: none;
+
+  @media screen and (max-width: 960px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 20;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    margin-left: 0.5rem;
+  }
 `;
 
-export const MobileMenu = styled.ul`
+export const NavMenu = styled.ul`
   display: flex;
   align-items: center;
   list-style: none;
   text-align: center;
+  font-size: 1.5rem;
+  margin-right: 1.5rem;
 
   @media screen and (max-width: 960px) {
     display: flex;
@@ -45,9 +84,85 @@ export const MobileMenu = styled.ul`
     height: 90vh;
     position: absolute;
     top: 80px;
-    left: ${({click}) => (click ? 0 : '-100%')};
+    left: ${({mobileNav}) => (mobileNav ? 0 : '-100%')};
     opacity: 1;
     transition: all 0.5s ease;
     background: #59A371;
   }
 `;
+
+export const NavItem = styled.li`
+  height: 80px;
+  border-bottom: 2px solid transparent;
+
+  &:hover {
+    border-bottom: 2px solid #4b59f7;
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+
+    &:hover {
+      border: none;
+    }
+  }
+`;
+
+export const NavLinks = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0.5rem 1rem;
+  height: 100%;
+
+  @media screen and (max-width: 960px) {
+    text-align: center;
+    padding: 2rem;
+    width: 100%;
+    display: table;
+
+    &:hover {
+      color: #4b59f7;
+      transition: all 0.3s ease;
+    }
+  }
+`;
+
+export const UserIcon = styled(VscAccount)`
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    z-index: 2;
+`;
+
+export const UserOptions = styled.ul`
+  /* display: flex; really want this to work and not the line below */
+  display: ${({userDropdown}) => (userDropdown ? 'flex' : 'none')};
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  font-size: 1.5rem;
+  flex-direction: column;
+  width: 30%;
+  height: 20vh;
+  position: absolute;
+  right: ${({userDropdown}) => (userDropdown ? '0' : '-100%')};
+  top: ${({userDropdown}) => (userDropdown ? '80px' : '-100%')};
+  opacity: 1;
+  transition: all 0.5s ease;
+  background: #59A371;
+  z-index: 0;
+  color: #fff;
+
+  @media screen and (max-width: 960px) {
+    font-size: 1.1rem;
+    /* display: ${({userDropdown}) => (userDropdown ? 'flex' : 'none')};
+    overflow-y: hidden; */
+  }
+`;
+
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,95 @@
 import React, { useState } from 'react'
 import { FaTimes, FaBars } from 'react-icons/fa';
-import { HeaderContainer, Logo, MobileIcon, MobileMenu } from './Header.elements'
+import { IconContext } from 'react-icons/lib';
+import { 
+  HeaderContainer, 
+  HeaderWrapper, 
+  Logo, 
+  LogoIcon, 
+  MobileIcon, 
+  NavMenu,
+  NavItem, 
+  NavLinks,
+  UserOptions,
+  UserIcon
+} from './Header.elements'
 
 
 const Header = () => {
-  const [click, setClick] = useState(false)
+  const [mobileNav, setMobileNav] = useState(false)
+  const handleMobileNav = () => {
+    setUserDropdown(false) 
+    return setMobileNav(!mobileNav)
+  }
+   
+  const [userDropdown, setUserDropdown] = useState(false)
+  const handleUserDropdown = () => {
+    setMobileNav(false)
+    return setUserDropdown(!userDropdown)
+  }
 
-  const handleClick = () => setClick(!click)
+  const handleMenus = () => {
+    setUserDropdown(false) 
+    setMobileNav(false)
+  }
 
   return (
     <>
-      <HeaderContainer>
-        <Logo to='/'>
-          ForeFinder
-        </Logo> 
-        <MobileIcon onClick={handleClick}>
-          {click ? <FaTimes/> : <FaBars/>}
-        </MobileIcon> 
-        <MobileMenu onClick={handleClick} click={click}>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <HeaderContainer>
+          <HeaderWrapper userDropdown={userDropdown}>
+            <MobileIcon onClick={handleMobileNav} data-cy='mobile-menu'>
+              {mobileNav ? <FaTimes/> : <FaBars/>}
+            </MobileIcon> 
+            <Logo to='/dashboard' onClick={handleMenus}>
+              <LogoIcon data-cy='logo'/>
+              ForeFinder
+            </Logo> 
+            <NavMenu 
+              onClick={handleMenus} 
+              mobileNav={mobileNav}
+              userDropdown={userDropdown} 
+              >
+              {/* <NavItem data-cy='dash-link'>
+                <NavLinks to='/dashboard'>
+                  Home
+                </NavLinks>
+              </NavItem> */}
 
-        </MobileMenu>
-      </HeaderContainer>
+              <NavItem data-cy='form-link'>
+                <NavLinks to='/form'>
+                  Add Tee Time
+                </NavLinks>
+              </NavItem>
+              
+              {/* <NavItem data-cy='friends-link'>
+                <NavLinks to='/friends'>
+                  My Friends
+                </NavLinks>
+              </NavItem> */}
+
+              <NavItem data-cy='community-link'>
+                <NavLinks to='/community'>
+                  My Community
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+            <UserIcon 
+              onClick={handleUserDropdown} data-cy='user-dropdown'/>
+            <UserOptions 
+              onClick={handleUserDropdown} 
+              userDropdown={userDropdown} 
+              mobileNav={mobileNav}>
+                User Name
+              <NavItem data-cy='logout-link'>
+                <NavLinks to='/login'>
+                  Log Out
+                </NavLinks>
+              </NavItem>
+            </UserOptions>
+          </HeaderWrapper>
+        </HeaderContainer>
+      </IconContext.Provider>
     </>
   )
 }
