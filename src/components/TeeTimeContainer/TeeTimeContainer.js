bf21f6ecace470784300c2ed1f4755b2a13121b3
schlanjo <schlanjo@gmail.com>
2021-07-10 16:06:20 -0600

    Move visible box shadow from container to tee time

--- src/components/TeeTimeContainer/TeeTimeContainer.js
+++ src/components/TeeTimeContainer/TeeTimeContainer.js
@@ -5,17 +5,15 @@ import './TeeTimeContainer.css'
 
 const TTWrapper = styled.div`
   width: 100%;
-  height: 320px;
   display: flex;
   flex-direction: column;
   margin-bottom: 24px;
   border-radius: 8px;
-  box-shadow: 0 2px 12px rgba(0, 0, 0, .2);
 `
 
 const TTLabel = styled.h2`
   align-self: center;
-  margin: 16px 0;
+  margin-bottom: 16px;
   font-size: 1.25em;
   font-weight: 600;
 `
