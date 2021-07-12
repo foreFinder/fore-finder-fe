663b96d9ff55222d8dc83bd20b2acfed7f53a6a1
schlanjo <schlanjo@gmail.com>
2021-07-12 17:42:02 -0600

    Render dashboard via '/dashboard', set redirect for root

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from '../Header/Header'
 import Dashboard from '../Dashboard/Dashboard'
 import React, { useState, useEffect } from 'react' 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -18,14 +18,16 @@ function App() {
       <Header screenWidth={screenWidth}/>
       <Switch>
         <Route 
-        exact path='/'
+        exact path='/dashboard'
           render={() => (
             <Dashboard
               // passing state
             />
           )}
         />
-
+        <Route exact path='/'>
+          <Redirect to='/dashboard' />
+        </Route>
       </Switch>
     </Router>
   )
