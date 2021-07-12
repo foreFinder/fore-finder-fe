7e8bc6614a9f3af6be33ed5b6a340155a595fafb
schlanjo <schlanjo@gmail.com>
2021-07-12 09:02:08 -0600

    Move TypeSelector to own component, render based on window width

--- src/components/TeeTimeContainer/TeeTimeContainer.js
+++ src/components/TeeTimeContainer/TeeTimeContainer.js
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 import TeeTime from '../TeeTime/TeeTime'
+import TypeSelector from './TypeSelector/TypeSelector'
 
-import styled from 'styled-components';
 import './TeeTimeContainer.css'
 
 const StyleWrapper = styled.div`
@@ -40,44 +41,15 @@ const ContainerTitle = styled.div`
   }
 `
 
-const TypeSelector = styled.span`
-  background-color: rgb(241, 243, 244);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-bottom-left-radius: 16px;
-  border-bottom-right-radius: 16px;
-  // border-top: solid 1px rgb(213, 214, 215);
-
-  .type-select-btn {
-    background-color: unset;
-    padding: unset;
-    border: unset;
-    font-size: 1.15em;
-    font-weight: 500;
-    cursor: pointer;
-  }
-
-  .type-select-btn:nth-child(1) {
-    margin-right: 6px;
-  }
-
-  .type-select-btn:nth-child(2) {
-    margin-left: 6px;
-  }
-
-  .unselected {
-    color: rgba(0, 0, 0, .25);
-
-`
-
 const TeeTimes = styled.div`
-  padding: 20px 20px 0 20px;
-  overflow: auto;
+  @media only screen and (min-width: 768px) {
+    padding: 20px 20px 0 20px;
+    overflow: auto;
+  }
 `
 
 const TeeTimeContainer = ({ title }) => {
-  const [allSelected, setAllSelected] = useState(false)
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   const getEventType = () => {
     if (title === 'Committed Tee Times') {
@@ -87,37 +59,24 @@ const TeeTimeContainer = ({ title }) => {
     }
   }
 
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setWindowWidth(window.innerWidth)
+    })
+  }, [])
+
   return (
     <StyleWrapper>
       <ContainerTitle>
         <h2>{title}</h2>
       </ContainerTitle>
+      {(title === 'Available Tee Times' && windowWidth < 768) && <TypeSelector mobile={true} />}
       <TeeTimes>
         <TeeTime type={getEventType()}/>
       </TeeTimes>
-      <TypeSelector>
-        {title === 'Available Tee Times' &&
-          <>
-            <button 
-              className={allSelected ? 'type-select-btn unselected' : 'type-select-btn'}
-              onClick={() => {
-                if (allSelected) setAllSelected(!allSelected)
-              }}
-            >
-              Friends
-            </button> 
-            | 
-            <button
-              className={allSelected ? 'type-select-btn' : 'type-select-btn unselected'}
-              onClick={() => {
-                if (!allSelected) setAllSelected(!allSelected)
-              }}
-            >
-              All
-            </button>
-          </>
-        }
-      </TypeSelector>
+      <div className='type-select-con'>
+        {(title === 'Available Tee Times' && windowWidth >= 768) && <TypeSelector mobile={false} />}
+      </div>
     </StyleWrapper>
   )
 }
