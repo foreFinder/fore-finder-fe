a5acb0815f9701bbb96ad35fcf3518a61844b908
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-07-12 11:55:36 -0600

    Remove fragment for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -34,64 +34,64 @@ const Header = ({screenWidth}) => {
   }
 
   return (
-    <>
-      <IconContext.Provider value={{ color: '#fff' }}>
-        <HeaderContainer>
-          <HeaderWrapper userDropdown={userDropdown}>
-            <MobileIcon onClick={handleMobileNav} data-cy='mobile-menu'>
-              {mobileNav ? <FaTimes/> : <FaBars/>}
-            </MobileIcon> 
-            <Logo to='/dashboard' onClick={handleMenus}>
-              <LogoIcon data-cy='logo'/>
-              ForeFinder
-            </Logo> 
-            <NavMenu 
-              onClick={handleMenus} 
-              mobileNav={mobileNav}
-              userDropdown={userDropdown} 
-              >
-              {/* <NavItem data-cy='dash-link'>
-                <NavLinks to='/dashboard'>
-                  Home
-                </NavLinks>
-              </NavItem> */}
+    
+    <IconContext.Provider value={{ color: '#fff' }}>
+      <HeaderContainer>
+        <HeaderWrapper userDropdown={userDropdown}>
+          <MobileIcon onClick={handleMobileNav} data-cy='mobile-menu'>
+            {mobileNav ? <FaTimes/> : <FaBars/>}
+          </MobileIcon> 
+          <Logo to='/dashboard' onClick={handleMenus}>
+            <LogoIcon data-cy='logo'/>
+            ForeFinder
+          </Logo> 
+          <NavMenu 
+            onClick={handleMenus} 
+            mobileNav={mobileNav}
+            userDropdown={userDropdown} 
+            >
+            {/* <NavItem data-cy='dash-link'>
+              <NavLinks to='/dashboard'>
+                Home
+              </NavLinks>
+            </NavItem> */}
 
-              <NavItem data-cy='form-link'>
-                <NavLinks to='/form'>
-                  Add Tee Time
-                </NavLinks>
-              </NavItem>
-              
-              {/* <NavItem data-cy='friends-link'>
-                <NavLinks to='/friends'>
-                  My Friends
-                </NavLinks>
-              </NavItem> */}
-          {screenWidth <= 480 && 
-              <NavItem data-cy='community-link'>
-                <NavLinks to='community'>
-                  My Community
-                </NavLinks>
-              </NavItem>
-              }
-            </NavMenu>
-            <UserIcon 
-              onClick={handleUserDropdown} data-cy='user-dropdown'/>
-            <UserOptions 
-              onClick={handleUserDropdown} 
-              userDropdown={userDropdown} 
-              mobileNav={mobileNav}>
-                User Name
-              <NavItem data-cy='logout-link'>
-                <NavLinks to='/login'>
-                  Log Out
-                </NavLinks>
-              </NavItem>
-            </UserOptions>
-          </HeaderWrapper>
-        </HeaderContainer>
-      </IconContext.Provider>
-    </>
+            <NavItem data-cy='form-link'>
+              <NavLinks to='/form'>
+                Add Tee Time
+              </NavLinks>
+            </NavItem>
+            
+            {/* <NavItem data-cy='friends-link'>
+              <NavLinks to='/friends'>
+                My Friends
+              </NavLinks>
+            </NavItem> */}
+        {screenWidth <= 480 && 
+            <NavItem data-cy='community-link'>
+              <NavLinks to='community'>
+                My Community
+              </NavLinks>
+            </NavItem>
+            }
+          </NavMenu>
+          <UserIcon 
+            onClick={handleUserDropdown} data-cy='user-dropdown'/>
+          <UserOptions 
+            onClick={handleUserDropdown} 
+            userDropdown={userDropdown} 
+            mobileNav={mobileNav}>
+              User Name
+            <NavItem data-cy='logout-link'>
+              <NavLinks to='/login'>
+                Log Out
+              </NavLinks>
+            </NavItem>
+          </UserOptions>
+        </HeaderWrapper>
+      </HeaderContainer>
+    </IconContext.Provider>
+
   )
 }
 
