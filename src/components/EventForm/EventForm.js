e0d4eb987fc5da738f91c234eb59545e91456501
Eric Rabun <errabun@gmail.com>
2021-07-12 08:47:16 -0600

    Add radio button selections for number of holes

--- src/components/EventForm/EventForm.js
+++ src/components/EventForm/EventForm.js
@@ -6,11 +6,13 @@ function EventForm() {
   const [date, setDate] = useState('')
   const [teeTime, setTeeTime] = useState('')
   const [openSpots, setOpenSpots] = useState('')
-  const []
+  const [friendList, setFriendList] = useState([])
+  const [numHoles, setNumHoles] = useState('')
 
   return(
     <>
       <form>
+        <h2>Add a Tee Time</h2>
         <label for='Date'>Date of tee time:</label>
         <input
           type='date'
@@ -19,21 +21,21 @@ function EventForm() {
           value={date}
           onChange={(event) => setDate(event.target.value)}
         />
-        <label for='Tee Time'>Tee Time:</label>
+        <label for='teeTime'>Tee Time:</label>
         <input
           type='time'
           name='Tee Time'
-          id='Tee Time'
+          id='teeTime'
           min='07:00'
           max='17:00'
           value={teeTime}
           onChange={(event) => setTeeTime(event.target.value)}
           required
         />
-        <label for='num players'>Open Positions:</label>
+        <label for='numPlayers'>Open Positions:</label>
         <select
           name='num players'
-          id='num players'
+          id='numPlayers'
           value={openSpots}
           onChange={(event) => setOpenSpots(event.target.value)}
         >
@@ -41,6 +43,27 @@ function EventForm() {
           <option value='2'>2</option>
           <option value='3'>3</option>
         </select>
+        <div>
+          <label for='numHoles'>Number of Holes:</label>
+          <label for='18'>18</label>
+          <input 
+            type='radio'
+            id='18'
+            name='numHoles'
+            value='18'
+            onClick={(event) => setNumHoles(event.target.value)}
+          />
+          <label for='9'>9</label>
+          <input 
+            type='radio'
+            id='9'
+            name='numHoles'
+            value='9'
+            onClick={(event) => setNumHoles(event.target.value)}
+          />
+        </div>
+        
+
       </form>
     </>
   )
