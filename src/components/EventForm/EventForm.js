cfa147901010ab7b030c111743aeb5132ef99052
Chris Anderson <mistercanderson@gmail.com>
2021-07-12 17:38:44 -0600

    Style form layout

--- src/components/EventForm/EventForm.js
+++ src/components/EventForm/EventForm.js
@@ -43,116 +43,139 @@ function EventForm() {
   };
 
   return (
-    <form>
+    <form onSubmit={(e) => e.preventDefault()}>
       <h2>Add a Tee Time</h2>
-      <label for='golfCourse'>Which golf course?</label>
-      <select
-        name='golfCourse'
-        id='golfCourse'
-        value={golfCourse}
-        onChange={(event) => setGolfCourse(event.target.value)}
-      >
-        <option>-* Please Select a Course *-</option>
-        {courses.map((course) => {
-          return <option value={course.name}>{course.name}</option>;
-        })}
-      </select>
-      <label for='Date'>Date of tee time:</label>
-      <input
-        type='date'
-        name='Date'
-        id='Date'
-        value={date}
-        onChange={(event) => setDate(event.target.value)}
-        required
-      />
-      <label for='teeTime'>Tee Time:</label>
-      <input
-        type='time'
-        name='Tee Time'
-        id='teeTime'
-        min='07:00'
-        max='17:00'
-        value={teeTime}
-        onChange={(event) => setTeeTime(event.target.value)}
-        required
-      />
-      <label for='numPlayers'>Open Positions:</label>
-      <select
-        name='num players'
-        id='numPlayers'
-        value={openSpots}
-        onChange={(event) => setOpenSpots(event.target.value)}
-      >
-        <option value='1'>1</option>
-        <option value='2'>2</option>
-        <option value='3'>3</option>
-      </select>
-      <div>
-        <label for='numHoles'>Number of Holes:</label>
-        <label for='18'>18</label>
-        <input
-          type='radio'
-          id='18'
-          name='numHoles'
-          value='18'
-          onClick={(event) => setNumHoles(event.target.value)}
-        />
-        <label for='9'>9</label>
-        <input
-          type='radio'
-          id='9'
-          name='numHoles'
-          value='9'
-          onClick={(event) => setNumHoles(event.target.value)}
-        />
-      </div>
-      <div>
-        <label for='publicStatus'>Public or Private Invite?</label>
-        <label for='public'>Public</label>
-        <input
-          type='radio'
-          id='public'
-          name='publicStatus'
-          value='public'
-          defaultChecked
-          onClick={() => setIsPrivate(false)}
-        />
-        <label for='private'>Private</label>
-        <input
-          type='radio'
-          id='private'
-          name='publicStatus'
-          value='private'
-          onClick={() => setIsPrivate(true)}
-        />
-      </div>
-      {isPrivate && (
-        <div>
-          {userFriends.map((friend) => {
-            return (
-              <div>
-                <input
-                  className='friends'
-                  type='checkbox'
-                  value={friend}
-                  key={friend}
-                  onClick={addFriendToInvite}
-                  disabled={allFriends ? true : false}
-                />
-                <label for={friend}>{friend}</label>
-              </div>
-            );
-          })}
+      <div className='form-components'>
+        <label for='golfCourse'>
+          Golf Course:
+          <select
+            name='golfCourse'
+            id='golfCourse'
+            value={golfCourse}
+            onChange={(event) => setGolfCourse(event.target.value)}
+          >
+            <option>-* Please Select a Course *-</option>
+            {courses.map((course) => {
+              return <option value={course.name}>{course.name}</option>;
+            })}
+          </select>
+        </label>
+        <label for='Date'>
+          Date:
           <input
-            type='checkbox'
-            id='allFriends'
-            value={[...userFriends]}
-            onClick={inviteAllFriends}
+            type='date'
+            name='Date'
+            id='Date'
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+            required
           />
-          <label for='allFriends'>Invite All Friends</label>
-        </div>
-      )}
+        </label>
+        <label for='teeTime'>
+          Tee Time:
+          <input
+            type='time'
+            name='Tee Time'
+            id='teeTime'
+            min='07:00'
+            max='17:00'
+            value={teeTime}
+            onChange={(event) => setTeeTime(event.target.value)}
+            required
+          />
+        </label>
+        <label for='numPlayers'>
+          Total Players (including you):
+          <select
+            name='num players'
+            id='numPlayers'
+            value={openSpots}
+            onChange={(event) => setOpenSpots(event.target.value)}
+          >
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+          </select>
+        </label>
+        <label for='numHoles'>
+          Number of Holes:
+          <label for='18'>
+            18
+            <input
+              type='radio'
+              id='18'
+              name='numHoles'
+              value='18'
+              onClick={(event) => setNumHoles(event.target.value)}
+            />
+          </label>
+          <label for='9'>
+            9
+            <input
+              type='radio'
+              id='9'
+              name='numHoles'
+              value='9'
+              onClick={(event) => setNumHoles(event.target.value)}
+            />
+          </label>
+        </label>
+        <label for='publicStatus'>
+          Public or Private:
+          <label for='public'>
+            Public
+            <input
+              type='radio'
+              id='public'
+              name='publicStatus'
+              value='public'
+              defaultChecked
+              onClick={() => setIsPrivate(false)}
+            />
+          </label>
+          <label for='private'>
+            Private
+            <input
+              type='radio'
+              id='private'
+              name='publicStatus'
+              value='private'
+              onClick={() => setIsPrivate(true)}
+            />
+          </label>
+        </label>
+        {isPrivate && (
+          <>
+            <p>Friends:</p>
+            <div className='friend-list-container'>
+              {userFriends.map((friend) => {
+                return (
+                  <div className='friend-list'>
+                    <input
+                      className='friends'
+                      type='checkbox'
+                      value={friend}
+                      key={friend}
+                      onClick={addFriendToInvite}
+                      disabled={allFriends ? true : false}
+                    />
+                    <label for={friend}>{friend}</label>
+                  </div>
+                );
+              })}
+            </div>
+            <div className='friend-list'>
+              <input
+                type='checkbox'
+                id='allFriends'
+                value={[...userFriends]}
+                onClick={inviteAllFriends}
+              />
+              <label for='allFriends'>Invite All Friends</label>
+            </div>
+          </>
+        )}
+      </div>
       <button /* onClick needs to send post with info*/>Create Tee Time</button>
     </form>
   );
