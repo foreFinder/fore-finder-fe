bf5089488b9d345a97e6c402f5731273dd411c49
schlanjo <schlanjo@gmail.com>
2021-07-12 18:36:22 -0600

    Establish state setting for event type select

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,29 @@
+import { useState } from 'react'
+
 import TeeTimeContainer from '../TeeTimeContainer/TeeTimeContainer'
 import './Dashboard.css'
 
 const Dashboard = () => {
+  const [teeTimeType, setTeeTimeType] = useState('committed')
+
   return (
     <div className='dashboard'>
       <h1 className='dashboard-title'>My Tee Times</h1>
-      <span className='event-type-select'><h2>Committed</h2> | <h2>Available</h2></span>
+      <span className='event-type-select'>
+        <button
+          className={teeTimeType === 'committed' ? 'event-type-btn' : 'event-type-btn unselected'}
+          onClick={() => { if (teeTimeType === 'available') setTeeTimeType('committed')}}
+        >
+          Committed
+        </button> 
+        | 
+        <button
+          className={teeTimeType === 'available' ? 'event-type-btn' : 'event-type-btn unselected'}
+          onClick={() => { if (teeTimeType === 'committed') setTeeTimeType('available')}}
+        >
+          Available
+        </button>
+      </span>
       <div className='tt-containers'>
         <TeeTimeContainer title='Committed Tee Times' />
         <TeeTimeContainer title='Available Tee Times' />
