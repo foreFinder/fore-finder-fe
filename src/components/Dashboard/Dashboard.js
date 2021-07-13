2efeeb1b34851821ee1c66ee576d7e42549059e4
schlanjo <schlanjo@gmail.com>
2021-07-12 18:03:19 -0600

    Add dashboard title and event type select

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import './Dashboard.css'
 const Dashboard = () => {
   return (
     <div className='dashboard'>
+      <h1 className='dashboard-title'>My Tee Times</h1>
+      <span className='event-type-select'><h2>Committed</h2> | <h2>Available</h2></span>
       <div className='tt-containers'>
         <TeeTimeContainer title='Committed Tee Times' />
         <TeeTimeContainer title='Available Tee Times' />
