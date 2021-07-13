5968ae9e15f50596bb19acbe4e667e4dc9371f3b
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-07-13 00:58:04 -0600

    Add friend and community toggle to player list

--- src/components/PlayerList/PlayerList.js
+++ src/components/PlayerList/PlayerList.js
@@ -4,17 +4,17 @@ import { players } from '../../APICalls/sampleData'
 import PlayerCard from '../PlayerCard/PlayerCard'
 
 const PlayerList = () => {
-  const [click, setClick] = useState(false)
+  const [click, setClick] = useState(true)
 
   const handleClick = () => setClick(!click)
 
   const currentUser = players.find((player) => player.id === 2); // need to make this dynamic to match what user is logged in
-  const userFriends = currentUser.friends.map((friendId) => {
-    return players.find((player) => player.id === friendId).name;
+  const userFriends = currentUser.friends.map(friendId => {
+    return players.find((player) => player.id === friendId);
   });
   
-  const mapPlayers = () => {
-    return players.map(p => (
+  const mapPlayers = (type) => {
+    return type.map(p => (
       <PlayerCard
         key={p.id}
         playerInfo={p}
@@ -32,13 +32,13 @@ const PlayerList = () => {
           Friends
         </button>
         <button
-          className={click ? 'friend-type-btn' : 'friend-type-btn unselected'}
+          className={click ? 'friend-type-btn unselected' : 'friend-type-btn'}
           onClick={handleClick}
         >
           Community
         </button>
       </span>
-      {mapPlayers()}
+      {click ? mapPlayers(userFriends) : mapPlayers(players)}
     </aside>
   )
 }
