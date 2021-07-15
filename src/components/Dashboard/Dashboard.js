3750ce963fafc3e82852a40362a670e780d383a1
schlanjo <schlanjo@gmail.com>
2021-07-14 22:34:27 -0600

    Fix merge conflicts after rebase with main

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import TeeTimeContainer from '../TeeTimeContainer/TeeTimeContainer'
 import './Dashboard.css'
 
-const Dashboard = ({ screenWidth }) => {
+const Dashboard = ({ events, screenWidth }) => {
+  const [availableTeeTimes, setAvailableTeeTimes] = useState([])
+  const [committedTeeTimes, setCommittedTeeTimes] = useState([])
   const [teeTimeType, setTeeTimeType] = useState('committed')
+
+  const getAvailable = () => {
+    return events.filter(event => {
+      return (event.attributes.invitees.includes(1) || !event.attributes.private)
+    })
+  }
+
+  const getCommitted = () => {
+    return events.filter(event => event.attributes.players.includes(1))
+  }
+  
+  useEffect(() => {
+    setAvailableTeeTimes(getAvailable())
+    setCommittedTeeTimes(getCommitted())
+  }, [])
+
+  console.log(committedTeeTimes)
 
   return (
     <div className='dashboard'>
@@ -27,13 +46,15 @@ const Dashboard = ({ screenWidth }) => {
       </span>
       {screenWidth >= 768 && 
         <div className='tt-containers'>
-          <TeeTimeContainer title='Committed Tee Times' windowWidth={screenWidth} />
-          <TeeTimeContainer title='Available Tee Times' windowWidth={screenWidth}/>
+          <TeeTimeContainer title='Committed Tee Times' events={committedTeeTimes} windowWidth={screenWidth} />
+          <TeeTimeContainer title='Available Tee Times' events={availableTeeTimes} windowWidth={screenWidth}/>
         </div>
       }
       {teeTimeType === 'committed'
-        ? (screenWidth < 768) && <TeeTimeContainer title='Committed Tee Times' windowWidth={screenWidth} />
-        : (screenWidth < 768) && <TeeTimeContainer title='Available Tee Times' windowWidth={screenWidth}/>
+        ? (screenWidth < 768) && 
+          <TeeTimeContainer title='Committed Tee Times' events={committedTeeTimes} windowWidth={screenWidth} />
+        : (screenWidth < 768) && 
+          <TeeTimeContainer title='Available Tee Times' events={availableTeeTimes} windowWidth={screenWidth}/>
       }
     </div>
   )
