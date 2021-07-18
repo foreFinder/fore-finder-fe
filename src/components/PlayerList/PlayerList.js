888cd93e0c62a44b4406812cf462bc2958445a25
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-07-17 18:13:11 -0600

    Fix merge conflicts

--- src/components/PlayerList/PlayerList.js
+++ src/components/PlayerList/PlayerList.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './PlayerList.css'
 import PlayerCard from '../PlayerCard/PlayerCard'
+import { getCurrentUser, getUserFriends } from '../../utilities'
+import { players } from '../../APICalls/sampleData' // gonna fetch through app and pass into here
 
 const PlayerList = ({screenWidth, players, friends, handleFriends}) => {
   const [playerType, setPlayerType] = useState('friends')  
@@ -18,6 +20,7 @@ const PlayerList = ({screenWidth, players, friends, handleFriends}) => {
 
   return (
     <aside className={screenWidth > 480 ? 'player-list-desktop' : 'player-list-mobile'}>
+<<<<<<< HEAD
       <span className={screenWidth > 480 ? 'player-type-select-desktop' : 'player-type-select-mobile'}>
         <button
           className={playerType === 'friends' ? 'player-type-btn' : 'player-type-btn unselected'}
@@ -33,6 +36,21 @@ const PlayerList = ({screenWidth, players, friends, handleFriends}) => {
       <ul className='player-list-wrapper'>
         {playerType === 'friends' ? mapPlayers(friends) : mapPlayers(players)}
       </ul>
+=======
+      <span className='player-type-select'>
+        <button
+          className={playerType === 'friends' ? 'friend-type-btn' : 'friend-type-btn unselected'}
+          onClick={() => { if (playerType === 'community') setPlayerType('friends')}}        >
+          Friends
+        </button>
+        <button
+          className={playerType === 'community' ? 'friend-type-btn' : 'friend-type-btn unselected'}
+          onClick={() => { if (playerType === 'friends') setPlayerType('community')}}        >
+          Community
+        </button>
+      </span>
+      {playerType === 'friends' ? mapPlayers(userFriends) : mapPlayers(players)}
+>>>>>>> df1a7ed (Add styling for mobile view)
     </aside>
   )
 }
