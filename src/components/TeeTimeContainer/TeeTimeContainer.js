1011cf67a5e5d4f03dcdd19c2958796940dbdf1a
Chris Anderson <mistercanderson@gmail.com>
2021-07-18 12:42:08 -0600

    Fix mobile styling & add no-invites message

--- src/components/TeeTimeContainer/TeeTimeContainer.js
+++ src/components/TeeTimeContainer/TeeTimeContainer.js
@@ -1,61 +1,96 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback } from 'react';
 
-import TeeTime from '../TeeTime/TeeTime'
-import InviteTypeSelect from './InviteTypeSelect/InviteTypeSelect'
+import TeeTime from '../TeeTime/TeeTime';
+import InviteTypeSelect from './InviteTypeSelect/InviteTypeSelect';
+import { Link } from 'react-router-dom';
 
-import './TeeTimeContainer.css'
+import './TeeTimeContainer.css';
 
-const TeeTimeContainer = ({ title, events, windowWidth, handleInviteAction }) => {
-  const [privateInvites, setPrivateInvites] = useState([])
-  const [invitesToDisplay, setInvitesToDisplay] = useState('private')
+const TeeTimeContainer = ({
+  title,
+  events,
+  windowWidth,
+  handleInviteAction,
+}) => {
+  const [privateInvites, setPrivateInvites] = useState([]);
+  const [invitesToDisplay, setInvitesToDisplay] = useState('private');
 
   const getEventType = useCallback(() => {
     if (title === 'Committed Tee Times') {
-      return 'committed'
+      return 'committed';
     } else if (title === 'Available Tee Times') {
-      return 'available'
+      return 'available';
     }
-  }, [title])
+  }, [title]);
 
   const getTeeTimes = () => {
-    let teeTimes = events
+    let teeTimes = events;
 
     if (getEventType() === 'available' && invitesToDisplay === 'private') {
-      teeTimes = privateInvites
+      teeTimes = privateInvites;
     }
 
-    return teeTimes.map(teeTime => {
+    return teeTimes.map((teeTime) => {
       return (
         <TeeTime
-          key={teeTime.id} 
-          type={getEventType()} 
+          key={teeTime.id}
+          type={getEventType()}
           event={teeTime}
-          handleInviteAction={handleInviteAction} 
+          handleInviteAction={handleInviteAction}
         />
-      )
-    })
+      );
+    });
+  };
+
+  const displayNoInviteMessage = () => {
+    return (
+      <p className='no-invites-card hidden'>
+        There are currently no tee time invitations from{' '}
+        {invitesToDisplay === 'private'
+          ? 'your friends'
+          : 'the ForeFinder community'}
+        . Would you like to{' '}
+        <Link className='no-invites-link' to='/event-form'>
+          create an event
+        </Link>
+        ?
+      </p>
+    );
+  };
+
+  const revealNoInviteMessage = (message) => {
+    setTimeout(() => message.classList.remove('hidden'), 1000)
   }
 
   useEffect(() => {
     if (getEventType() === 'available') {
-      setPrivateInvites(events.filter(event => event.attributes.private))
+      setPrivateInvites(events.filter((event) => event.attributes.private));
     }
-  }, [events, getEventType])
+    const noInviteMessages = Array.from(document.querySelectorAll('.no-invites-card'));
+    if (noInviteMessages) {
+      noInviteMessages.forEach(m => revealNoInviteMessage(m))
+    }
+
+  }, [events, getEventType]);
 
   return (
     <div className='tee-time-container'>
       <div className='container-title'>
         <h2>{title}</h2>
       </div>
-      {(title === 'Available Tee Times' && windowWidth < 768) && <InviteTypeSelect handleClick={setInvitesToDisplay} />}
+      {title === 'Available Tee Times' && windowWidth < 768 && (
+        <InviteTypeSelect handleClick={setInvitesToDisplay} />
+      )}
       <div className='tee-times'>
-        {getTeeTimes()}
+        {events.length ? getTeeTimes() : displayNoInviteMessage()}
       </div>
       <div className='type-select-con'>
-        {(title === 'Available Tee Times' && windowWidth >= 768) && <InviteTypeSelect handleClick={setInvitesToDisplay} />}
+        {title === 'Available Tee Times' && windowWidth >= 768 && (
+          <InviteTypeSelect handleClick={setInvitesToDisplay} />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TeeTimeContainer
+export default TeeTimeContainer;
