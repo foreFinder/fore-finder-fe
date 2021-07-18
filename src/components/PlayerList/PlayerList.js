dbf96c9984fb18faae388ff78851ce2eeda5e9fc
schlanjo <schlanjo@gmail.com>
2021-07-18 15:41:24 -0600

    Fix merge conflicts after rebase with main

--- src/components/PlayerList/PlayerList.js
+++ src/components/PlayerList/PlayerList.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import './PlayerList.css'
 import PlayerCard from '../PlayerCard/PlayerCard'
-import { getCurrentUser, getUserFriends } from '../../utilities'
-import { players } from '../../APICalls/sampleData' // gonna fetch through app and pass into here
 
 const PlayerList = ({screenWidth, players, friends, handleFriends}) => {
   const [playerType, setPlayerType] = useState('friends')  
@@ -20,7 +18,6 @@ const PlayerList = ({screenWidth, players, friends, handleFriends}) => {
 
   return (
     <aside className={screenWidth > 480 ? 'player-list-desktop' : 'player-list-mobile'}>
-<<<<<<< HEAD
       <span className={screenWidth > 480 ? 'player-type-select-desktop' : 'player-type-select-mobile'}>
         <button
           className={playerType === 'friends' ? 'player-type-btn' : 'player-type-btn unselected'}
@@ -36,22 +33,26 @@ const PlayerList = ({screenWidth, players, friends, handleFriends}) => {
       <ul className='player-list-wrapper'>
         {playerType === 'friends' ? mapPlayers(friends) : mapPlayers(players)}
       </ul>
-=======
       <span className='player-type-select'>
+      <span className={screenWidth > 480 ? 'player-type-select-desktop' : 'player-type-select-mobile'}>
         <button
-          className={playerType === 'friends' ? 'friend-type-btn' : 'friend-type-btn unselected'}
-          onClick={() => { if (playerType === 'community') setPlayerType('friends')}}        >
+          className={playerType === 'friends' ? 'player-type-btn' : 'player-type-btn unselected'}
+          onClick={() => { if (playerType === 'community') setPlayerType('friends')}}>
           Friends
         </button>
         <button
-          className={playerType === 'community' ? 'friend-type-btn' : 'friend-type-btn unselected'}
-          onClick={() => { if (playerType === 'friends') setPlayerType('community')}}        >
+          className={playerType === 'community' ? 'player-type-btn' : 'player-type-btn unselected'}
+          onClick={() => { if (playerType === 'friends') setPlayerType('community')}}>
           Community
         </button>
       </span>
       {playerType === 'friends' ? mapPlayers(userFriends) : mapPlayers(players)}
->>>>>>> df1a7ed (Add styling for mobile view)
+      <ul className='player-list-wrapper'>
+        {playerType === 'friends' ? mapPlayers(userFriends) : mapPlayers(players)}
+      </ul>
+      </span>
     </aside>
   )
 }
+
 export default PlayerList
