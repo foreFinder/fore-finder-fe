a13eb507cf5f25bb091a820e3fe1c7f741e8dcb2
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-07-18 13:58:55 -0600

    Add header navigation tests

--- cypress/integration/headerNavigation.spec.js
+++ cypress/integration/headerNavigation.spec.js
@@ -18,17 +18,49 @@ describe('Header Navigation', () => {
   })
 
   it('should display a logo that can take you the dashboard', () => {
+    // target logo, click it, check url
     cy.get('[data-cy=logo]')
       .should('be.visible').click()
     cy.url().should('eq', 'http://localhost:3000/dashboard')
   })
 
-  it.only('should have a route to form', () => {
+  it('should have a route to dashboard', () => {
+    // target dashboard link, click it, check url
+    cy.get('[data-cy=nav-menu]')
+    .get('[data-cy=dashboard-link]')
+    .should('be.visible')
+    .click()
+    cy.url().should('eq', 'http://localhost:3000/dashboard')
+  })
+
+  it('should have a route to form', () => {
+    // target form link, click it, check url
     cy.get('[data-cy=nav-menu]')
     .get('[data-cy=form-link]')
     .should('be.visible')
-    .get('[data-cy=community-link]')
-    .should('be.visible')
+    .click()
+    cy.url().should('eq', 'http://localhost:3000/event-form')
+  })
+
+  it('should have a route to community when not in desktop view', () => {
+    // target ham menu, target community link, click it, check url
+    cy.viewport(960, 540)
+    cy.get('[data-cy=ham-menu]').click()
+      .get('[data-cy=community-link]')
+      .should('be.visible')
+      .click()
+    cy.url().should('eq', 'http://localhost:3000/community')
+  })
+
+  it('should have a dropdown for user info and "logout"', () => {
+    // target user dropdown, click it, check it opened
+    cy.get('[data-cy=user-dropdown]').click()
+    cy.get('[data-cy=user-options]')
+      .should('be.visible')
+    // target logout link, click it, check url
+    cy.get('[data-cy=logout-link]').click()
+    cy.url().should('eq', 'http://localhost:3000/login')
+
   })
 
 })
