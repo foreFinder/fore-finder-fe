ceeb28becce28050c53f83a59c9583e7c8d20b11
Eric Rabun <errabun@gmail.com>
2021-08-02 09:13:17 -0600

    Change route for login page; fix styling for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -28,28 +28,41 @@ function Login() {
   }
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <label for='email' className='logLab'>Email:</label>
-      <input 
-        type='email' 
-        id='email'
-        name='email'
-        value={email} 
-        onChange={e => setEmail(e.target.value)}
-        required
-      />
-      <label for='password' className='logLab'>Password:</label>
-      <input
-        type='password'
-        id='password'
-        name='password'
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        required
-      />
-      <button onClick={() => handleLogin}>Login</button>
-      <button onClick={() => handleCreateProfile}>Create Profile</button>
-    </form>
+    <div className='form-container'>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className='form-title'>
+          <h2>Welcome to ForeFinder</h2>
+        </div>
+        <div className='form-components-container'>
+          <div className='form-components'>
+            <label for='email' className='logLab'>
+              Email:
+              <input 
+                type='email' 
+                id='email'
+                name='email'
+                value={email} 
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label for='password' className='logLab'>
+              Password:
+              <input
+                type='password'
+                id='password'
+                name='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+        </div>
+        <button onClick={() => handleLogin} className='form-submit'>Login</button>
+        <button onClick={() => handleCreateProfile} className='form-submit'>Create Profile</button>
+      </form>
+    </div>
   )
 }
 
