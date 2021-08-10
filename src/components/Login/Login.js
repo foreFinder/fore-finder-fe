6ad6f6286ccd297178c974825309cb2bce7d1e67
Eric Rabun <errabun@gmail.com>
2021-08-10 15:16:49 -0600

    Add Google oAuth login button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,11 +3,15 @@ import { useState } from 'react'
 import { Route, Link } from 'react-router-dom'
 import Dashboard from '../Dashboard/Dashboard'
 import CreateProfile from '../CreateProfile/CreateProfile'
+import { GoogleLogin } from 'react-google-login' 
 
 function Login() {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const client_id = '82670248652-tn3p68stodh94frteuucrfali5vk51ik.apps.googleusercontent.com'
+  const client_secret = '2L5M20Rqua4Xi8PGsZOlEWI5'
 
   const  handleLogin = () => {
     return (
@@ -27,6 +31,10 @@ function Login() {
     )
   }
 
+  const responseGoogle = response => {
+    console.log(response) 
+  }
+
   return (
     <div className='form-container'>
       <form onSubmit={(e) => e.preventDefault()}>
@@ -35,7 +43,7 @@ function Login() {
         </div>
         <div className='form-components-container'>
           <div className='form-components'>
-            <label for='email' className='logLab'>
+            <label htmlFor='email' className='logLab'>
               Email:
               <input 
                 type='email' 
@@ -46,7 +54,7 @@ function Login() {
                 required
               />
             </label>
-            <label for='password' className='logLab'>
+            <label htmlFor='password' className='logLab'>
               Password:
               <input
                 type='password'
@@ -60,6 +68,14 @@ function Login() {
           </div>
         </div>
         <div className='login-btn-container'>
+          <GoogleLogin
+            clientId={client_id}
+            buttonText='Login with Google'
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+            className='form-submit'
+          />
           <button onClick={() => handleLogin} className='form-submit'>Login</button>
           <Link to='/create-profile'>
             <button onClick={() => handleCreateProfile} className='form-submit'>Create Profile</button>
